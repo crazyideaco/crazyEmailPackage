e19066555d5db1a998a6a636c60c957a604fab53
mostafamhashem <mostafa.m.hashem.1@gmail.com>
2023-12-26 18:09:26 +0200

    add email template blade and send email controller

--- src/Services/SendMailService.php
+++ src/Services/SendMailService.php
@@ -1,11 +1,9 @@
 <?php
 
-namespace ApiAccounting\Services;
+namespace CrazyEmailPackage\SendEmail\Services;
 
-use Carbon\Carbon;
+
 use CrazyEmailPackage\SendEmail\Mail\SendMail;
-use Illuminate\Http\Request;
-use ApiHotel\Models\Hotel\Hotel;
 
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Config;
@@ -19,9 +17,10 @@ class SendMailService
     public function send_mail($mailData)
     {
         // Update the MAIL_FROM_ADDRESS dynamically
+        
         Config::set('mail.from.address', $mailData['sender_email']);
 
 
-        Mail::to($mailData['receiver_email'])->send(new SendMail($mailData, 'mail_notify'));
+        Mail::to($mailData['receiver_email'])->send(new SendMail($mailData, 'emails_templates.mail_notify'));
     }
 }
