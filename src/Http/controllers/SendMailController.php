3e908911fe7bb9791654f7a1df5c207804183da0
mostafamhashem <mostafa.m.hashem.1@gmail.com>
2023-12-27 17:49:56 +0200

    handle send_mail() function with request and handle require key in composer json

--- src/Http/controllers/SendMailController.php
+++ src/Http/controllers/SendMailController.php
@@ -12,10 +12,14 @@ use Illuminate\Validation\ValidationException;
 
 class SendMailController extends Controller
 {
-    public function send_mail($mailData)
+    public function send_mail(Request $request)
     {
         $send_mail_service = new SendMailService();
         $mailData['project_name'] = Config::get('app.name');
+        $mailData['sender_email'] = $request->input('sender_email');
+        $mailData['receiver_email'] = $request->input('receiver_email');
+        $mailData['subject'] = $request->input('subject');
+        $mailData['body'] = $request->input('body');
         $send_mail_service->send_mail($mailData);
     }
 }
